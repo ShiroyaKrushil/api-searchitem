03040682d42e459648faa0a517f80f5904ff7c5a
shiroya krushil <sdcard1130@gmail.com>
2023-05-13 12:41:36 +0530

    update

--- src/App.js
+++ src/App.js
@@ -1,23 +1,57 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+
+  const [Alldata, setAlldata] = useState([]);
+  const [serach, setSearch] = useState('');
+
+   const btn =()=>{
+    if(!serach)
+    {
+      fetch(`https://dummyjson.com/products/search?q=${serach}`).then((res) => {
+      return res.json();
+    }).then((data) => {
+
+      setAlldata(data.products);
+
+    })
+    }else{
+      alert('not found');
+    }
+   }
+
+  
+  console.log(Alldata);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container mt-5'>
+      <div className='row'>
+        <form>
+          <div class="form-group mb-3">
+            <input type="text" class="form-control " id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Search" onChange={(e) => setSearch(e.target.value)} />
+          </div>
+          <button type="button" class="btn btn-primary" onClick={btn}>Search</button>
+
+        </form>
+      </div>
+      <div className='row mt-5'>
+        {
+          Alldata.map((item) => {
+            return (
+              <>
+                <div class="card m-3 " style={{ width: '18rem' }}>
+                  <img class="card-img-top" src={item.thumbnail
+                  } alt="Card image cap" />
+                  <div class="card-body">
+                    <h5 class="card-title">{item.title}</h5>
+                  </div>
+                </div>
+              </>
+            )
+          })
+        }
+      </div>
     </div>
   );
 }
